b2549464174b19eced9c34145f5dc6067828b034
Nate Simonsen <61298685+natejsimonsen@users.noreply.github.com>
2024-01-03 14:43:04 -0800

    Update western.js

--- src/pages/western.js
+++ src/pages/western.js
@@ -1,1 +1,20 @@
+import React from "react";
 
+import { SudokuProvider } from "../context/sudokuContext";
+import { UserConfigProvider } from "../context/userConfigContext";
+import Layout from "../components/Layout";
+import Seo from "../components/Seo";
+import SudokuGrid from "../components/SudokuGrid";
+import SudokuToolbar from "../components/SudokuToolbar";
+
+const IndexPage = () => {
+  const arr = [];
+for (let i = 0; i < 10; i++) {
+    arr.push(Math.random() > .5 ? (i + 1) + "A" : (i + 1) + "B");
+}
+ return (
+    <p>{arr.toString()}</p>
+ )
+};
+
+export default IndexPage;
